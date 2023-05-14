9fa9f7ffd42f6bfc99f78092338ccea6cd18688e
azoozpr6 <81915413+azoozpr6@users.noreply.github.com>
2023-05-14 14:57:25 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function Skills(){
+    window.scrollTo(0,475.20001220703125)
+}
+function Aboutme(){
+    window.scrollTo(0,1084.800048828125
+        )
+}
+function github(){
+    window.open("https://github.com/azoozpr6?tab=repositories");
+}
